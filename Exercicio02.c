dd5091cc3175a7606a3bd3fab4a5fcf3593bdbed
Eduardo Emanuel <70245046+edurib17@users.noreply.github.com>
2021-09-19 11:19:41 -0300

    Adicionando descrição ao exercício

--- Exercicio02.c
+++ Exercicio02.c
@@ -1,3 +1,6 @@
+/*
+Escreva um algoritmo para calcular e imprimir o valor do salário bruto de um vendedor de imóveis. O vendedor recebe um valor fixo mensal além de uma comissão de 6% sobre o total das vendas do mês. O algoritmo deve ler o valor do salário fixo e do total das vendas.
+*/
 #include <stdio.h>
  
 int main(void)
